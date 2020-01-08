1d56e6a1db84b70c22cc9285831d2e97a58816cd
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-01-08 21:06:31 +0300

    Add files via upload

---
+++ finalproject/finalproject/RenewPasswordPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace finalproject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RenewPasswordPage : ContentPage
+    {
+        public RenewPasswordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ finalproject/finalproject/SignUpPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using finalproject;
+
+namespace finalproject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+        private void Register_OnClicked(object sender, EventArgs e)
+        {
+            //Preferences.Set("mailaddress", "password");
+
+            if (string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Text))
+                DisplayAlert("Empty Values", "Please enter E-mail address and Password", "OK");
+            else
+            {
+                Preferences.Set(email.Text, password.Text);
+                DisplayAlert("Sign Up Success", "", "Ok");
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+    }
+}
