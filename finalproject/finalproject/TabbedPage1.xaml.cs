32c82c0605eb9312a79c514cdf3505624125bcea
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-01-09 00:04:05 +0300

    Add files via upload

--- finalproject/finalproject/TabbedPage1.xaml.cs
+++ finalproject/finalproject/TabbedPage1.xaml.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace finalproject
 {
@@ -16,5 +16,12 @@ namespace finalproject
         {
             InitializeComponent();
         }
+
+        private async void Horror_Clicked(object sender, EventArgs e)
+        {
+            //TapGestureRecognizer.NumberOfTapsRequiredProperty.Equals(2);
+            await Navigation.PushAsync(new HorrorListPage());
+        }
+
     }
 }
