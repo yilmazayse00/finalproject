6cb119a6f2da98abc0fecc357e20bf81abadae68
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-02-23 02:43:41 +0300

    Add files via upload

---
+++ finalproject/finalproject/finalproject/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace finalproject
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void SignInButton_OnClicked(object sender, EventArgs e)
+        {
+            string value = Preferences.Get(email.Text, "");
+
+            if (password.Text == value)
+            {
+                    await Navigation.PushAsync(new TabbedPage1());
+            }
+            else
+            {
+                var result = await DisplayAlert("Login Process", "Username and Password Dismatch!", "", "CANCEL");
+                if (result != true) // if it's equal to cancel
+                {
+                    return; // just return to the page and do nothing.
+                }
+            }
+        }
+        void OnTapSignUpLabel(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignUpPage());
+        }
+
+        void OnTapForgotPasswordLabel(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RenewPasswordPage());
+        }
+    }
+}
---
+++ finalproject/finalproject/finalproject/MarkupExtensions/ImageSourceExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace finalproject.MarkupExtension
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageSourceExtension:IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Source == null ? null : ImageSource.FromResource(Source);
+        }
+    }
+}
